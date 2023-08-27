57541cc9a05581d4ada34fe0f2a23f2cb31bdeb5
Ridham24 <ridhamgoel03@gmail.com>
2023-08-27 17:13:12 +0530

    Aug 27 POTD

---
+++ Aug27POTD.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+string reverseWord(string str)
+    {
+        // Your code goes here
+        reverse(str.begin(),str.end());
+        return str;
+    }
+int main(){
+
+return 0;
+}
