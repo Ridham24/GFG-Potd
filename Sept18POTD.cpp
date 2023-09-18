ba183a38ac967882604e5747812170fb14ab45dd
Ridham24 <ridhamgoel03@gmail.com>
2023-09-18 18:13:27 +0530

    Sept 18 POTD

---
+++ Sept18POTD.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isPowerofTwo(long long n){
+        
+        // Your code here 
+        if(n==0)
+        return false;
+        if((n&(n-1))==0)
+        return true;
+        return false;
+    }
+int main(){
+
+return 0;
+}
