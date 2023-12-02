e8945ef0923a86ee30cd6f9b0001384fb583093d
Ridham24 <ridhamgoel03@gmail.com>
2023-12-02 15:20:41 +0530

    Dec 02 POTD

---
+++ Dec02POTD.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int isRepresentingBST(int arr[], int N)
+    {
+        for(int i=0;i<N-1;i++)
+        {
+            if(arr[i]>=arr[i+1])
+            return 0;
+        }
+        return 1;
+    }
+int main(){
+
+return 0;
+}
