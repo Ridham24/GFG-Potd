7ae70936935101371da8b3af02c952d2064adc58
Ridham24 <ridhamgoel03@gmail.com>
2023-12-12 23:41:08 +0530

    Dec 12 POTD

--- Dec12POTD.cpp
+++ Dec12POTD.cpp
@@ -1,24 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
-int maxProduct(vector<int>& nums) {
-        int maxi1=0,maxi2=0,x=0;
-        for(int i=0;i<nums.size();i++)
+int check(int i,int j,vector<vector<int>>& m,vector<vector<int>>& dp)
+    {
+        if(j>=m[0].size())
+        return 0;
+        if(dp[i][j]!=-1)
+        return dp[i][j];
+        int a=check(i,j+1,m,dp)+m[i][j];
+        if(i>0)
+        a=max(a,check(i-1,j+1,m,dp)+m[i][j]);
+        if(i<m.size()-1)
+        a=max(a,check(i+1,j+1,m,dp)+m[i][j]);
+        return dp[i][j]=a;
+    }
+
+    int maxGold(int n, int m, vector<vector<int>> M)
+    {
+        int maxi=0;
+        vector<vector<int>> dp(n,vector<int> (m,-1));
+        for(int i=0;i<n;i++)
         {
-            if(maxi1<nums[i])
-            {
-                maxi1=nums[i];
-                x=i;
-            }
+            maxi=max(maxi,check(i,0,M,dp));
         }
-        nums[x]=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(maxi2<nums[i])
-            {
-                maxi2=nums[i];
-            }
-        }
-        return (maxi1-1)*(maxi2-1);
+        return maxi;
     }
 int main()
 {
