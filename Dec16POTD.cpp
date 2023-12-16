0d4e9ed6b6744ff8494b4e9e25dd145fd6019d96
Ridham24 <ridhamgoel03@gmail.com>
2023-12-17 00:02:11 +0530

    Dec 16 POTD

---
+++ Dec16POTD.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long int countStr(long long int n){
+    //complete the function here
+    return (n*n*n+3*n+2)/2;
+}
+int main(){
+
+return 0;
+}
